99d30b2701d47db2fbe7929f811a574a3185ecbd
zhengllingling <1072828530@qq.com>
2021-05-13 19:39:46 +0800

    feat 多入口以及对应输出配置

--- README.md
+++ README.md
@@ -10,8 +10,16 @@
 
 webpack --config 制定配置文件 默认为webpack.config.js
 
-entry
+entry 
+1.打包入口
+2.通过入口形成一个依赖树
+3.webpack是一个模块打包器，即在webpack中万物皆模块，js文件、css文件、图片都是模块
+4.支持多入口配置
+
 output
+1.打包输出
+2.根据多入口打包，webpack输出配置也可以使用占位符区分不同入口打包的输入
+
 mode
 modules
 plugins
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,24 @@
+// const path = require("path");
+
+// module.exports = {
+//     entry: "./src/index.js", 
+//     mode: "development",
+//     output: {
+//         path: path.join(__dirname,"dist"),
+//         filename: "bundle.js"
+//     }
+// }
+
 const path = require("path");
 
 module.exports = {
-    entry: "./src/index.js",
-    mode: "development",
+    entry:{
+        app: "./src/app.js",
+        search: "./src/search.js"
+    },
     output: {
-        path: path.join(__dirname,"dist"),
-        filename: "bundle.js"
-    }
+        path: path.join(__dirname, 'dist'),
+        filename: "[name].js"
+    },
+    mode: "development"
 }
